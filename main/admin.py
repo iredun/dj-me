d8894c5911a4af3e6f5a44657ca656d59a6673f4
Ivan <redunivan@yandex.ru>
2021-02-27 15:43:07 +0300

    ORM

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from main.models import Skill
 
-# Register your models here.
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'percent']
+    list_display_links = ['id', 'name', 'percent']
--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Skill(models.Model):
+    name = models.CharField(verbose_name="Название", max_length=100)
+    percent = models.IntegerField(verbose_name="Процент", validators=[
+        MinValueValidator(0),
+        MaxValueValidator(100)
+    ])
+
+    class Meta:
+        verbose_name = 'Навык'
+        verbose_name_plural = 'Навыки'
+
+    def __str__(self):
+        return f"{self.name} {self.percent}%"
