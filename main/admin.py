05a2da1b1837a99f3ab98c6a8ce6dd6d9c82c2c0
Ivan Redun <redunivan@yandex.ru>
2021-03-05 18:52:13 +0300

    Языки

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from main.models import Skill
+from main.models import Skill, Language
 
 
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'percent']
     list_display_links = ['id', 'name', 'percent']
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'desc', 'stars']
+    list_display_links = ['id', 'name']
--- main/models.py
+++ main/models.py
@@ -15,3 +15,16 @@ class Skill(models.Model):
 
     def __str__(self):
         return f"{self.name} {self.percent}%"
+
+
+class Language(models.Model):
+    name = models.CharField(verbose_name="Название", max_length=100)
+    desc = models.CharField(verbose_name="Описание", max_length=255)
+    stars = models.FloatField(verbose_name="Звезды")
+
+    class Meta:
+        verbose_name = 'Язык'
+        verbose_name_plural = 'Языки'
+
+    def __str__(self):
+        return self.name
