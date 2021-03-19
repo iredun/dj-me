c3ce87e30d4daff176f81a3f1d7daebbeea519fd
Ivan Redun <redunivan@yandex.ru>
2021-03-19 20:46:00 +0300

    Form

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from main import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('contact/', views.contact_form, name='contact_form'),
     path('projects/<int:pk>/', views.project_detail, name='project_detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from django.shortcuts import render
 
 from main.models import Skill, Language, Education, SocLink, Project
@@ -24,3 +26,13 @@ def project_detail(request, pk):
     }
 
     return render(request, 'main/project_detail.html', context)
+
+
+def contact_form(request):
+    context = {
+        'soc_links': SocLink.objects.all()
+    }
+    if request.method == 'POST':
+        pprint(request.POST)
+
+    return render(request, 'main/contact_form.html', context)
